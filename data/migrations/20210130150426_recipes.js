961b39e88d3c324643e2b6bc583c5e9c413b1dfa
Matthew Bokovitz <MattBokovitz@Matthews-MBP-2.fios-router.home>
2021-02-02 11:42:22 -0500

    updated database

--- data/migrations/20210130150426_recipes.js
+++ data/migrations/20210130150426_recipes.js
@@ -1,57 +1,16 @@
 exports.up = function (knex) {
-  return knex.schema
-    .createTable("recipes", (table) => {
-      table.increments("recipe_id");
-      table.string("recipe_name", 128).notNullable();
-      table.string("author", 128).notNullable();
-      table.string("category", 128).notNullable();
-      table.string("time", 128).notNullable();
-      table.string("img", 1000);
-      table.boolean("recipe_private").notNullable().defaultTo(false);
-    })
-    .createTable("ingredients", (table) => {
-      table.increments("ingredient_id");
-      table.string("ingredient_name", 128).notNullable();
-    })
-    .createTable("steps", (table) => {
-      table.increments("step_id");
-      table.string("step_text", 128).notNullable();
-      table.integer("step_order").notNullable();
-      table
-        .integer("recipe_id")
-        .unsigned()
-        .notNullable()
-        .references("recipe_id")
-        .inTable("recipes")
-        .onDelete("RESTRICT")
-        .onUpdate("RESTRICT");
-    })
-    .createTable("step_ingredients", (table) => {
-      table.increments("step_ingredient_id");
-      table
-        .integer("step_id")
-        .unsigned()
-        .notNullable()
-        .references("step_id")
-        .inTable("steps")
-        .onDelete("RESTRICT")
-        .onUpdate("RESTRICT");
-      table
-        .integer("ingredient_id")
-        .unsigned()
-        .notNullable()
-        .references("ingredient_id")
-        .inTable("ingredients")
-        .onDelete("RESTRICT")
-        .onUpdate("RESTRICT");
-      table.float("ingredient_quantity").notNullable();
-    });
+  return knex.schema.createTable("recipes", (table) => {
+    table.increments("recipe_id");
+    table.string("recipe_name", 128).notNullable();
+    table.string("author", 128).notNullable();
+    table.string("category", 128).notNullable();
+    table.string("time", 128).notNullable();
+    table.boolean("recipe_private").notNullable().defaultTo(false);
+    table.text("ingredients").notNullable();
+    table.text("steps").notNullable();
+  });
 };
 
 exports.down = function (knex) {
-  return knex.schema
-    .dropTableIfExists("step_ingredients")
-    .dropTableIfExists("steps")
-    .dropTableIfExists("ingredients")
-    .dropTableIfExists("recipes");
+  return knex.schema.dropTableIfExists("recipes");
 };
